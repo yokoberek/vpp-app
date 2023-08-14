b06654b6b28fb6eb478a367a5e2d2b37700a5cc7
antheiz <salupacode@gmail.com>
2023-08-15 03:52:27 +0700

    feat: update ploting and button filter by date

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint
 from flask_login import login_required
 from app import session
+import json
 
 main = Blueprint("main", __name__, template_folder="templates")
 
@@ -8,38 +9,33 @@ main = Blueprint("main", __name__, template_folder="templates")
 @main.route("/")
 @login_required
 def index():
-    # Plot 1 [emsdata(v10, v11)]
-    time_plot = session.execute("SELECT date FROM emsdata LIMIT 50")
-    plot_time = [value[0].strftime("%m/%d/%Y, %H:%M:%S") for value in time_plot]
-    array_v10 = session.execute("SELECT v10 FROM emsdata LIMIT 50")
-    data_v10 = [value[0] for value in array_v10]
-    array_v11 = session.execute("SELECT v11 FROM emsdata LIMIT 50")
-    data_v11 = [value[0] for value in array_v11]
-    # Plot 2 [emsdata(v12)]
-    array_v12 = session.execute("SELECT v12 FROM emsdata LIMIT 50")
-    data_v12 = [value[0] for value in array_v12]
-    # Plot 3 [emsdata(v13)]
-    array_v13 = session.execute("SELECT v13 FROM emsdata LIMIT 50")
-    data_v13 = [value[0] for value in array_v13]
-    # Plot 4
-    time_bill = session.execute("SELECT daytime FROM electrbill LIMIT 50")
-    bill_time = [value[0].strftime("%m/%d/%Y, %H:%M:%S") for value in time_bill]
-    array_bill1 = session.execute("SELECT billVpp1 FROM electrbill LIMIT 50")
-    bill_vpp1 = [value[0] for value in array_bill1]
-    array_bill2 = session.execute("SELECT billVpp2 FROM electrbill LIMIT 50")
-    bill_vpp2 = [value[0] for value in array_bill2]
-    array_bill3 = session.execute("SELECT billVpp3 FROM electrbill LIMIT 50")
-    bill_vpp3 = [value[0] for value in array_bill3]
+    # Plot 1
+    obj_data1 = session.execute("SELECT date, v10, v11 FROM emsdata ORDER BY date DESC").fetchall()
+    data_v10 = [[int(item[0].timestamp()) * 1000, item[1]] for item in obj_data1]
+    data_v11 = [[int(item[0].timestamp()) * 1000, item[2]] for item in obj_data1]
 
-    return render_template(
-        "main/index.html",
-        plot_time=plot_time,
-        data_v10=data_v10,
-        data_v11=data_v11,
-        data_v12=data_v12,
-        data_v13=data_v13,
-        bill_time=bill_time,
-        bill_vpp1=bill_vpp1,
-        bill_vpp2=bill_vpp2,
-        bill_vpp3=bill_vpp3,
-    )
+    # Plot 2
+    obj_data2 = session.execute("SELECT date, v12 FROM emsdata ORDER BY date DESC").fetchall()
+    data_v12 = [[int(item[0].timestamp()) * 1000, item[1]] for item in obj_data2]
+
+    # Plot 3
+    obj_data3 = session.execute("SELECT date, v13 FROM emsdata ORDER BY date DESC").fetchall()
+    data_v13 = [[int(item[0].timestamp()) * 1000, item[1]] for item in obj_data3]
+
+    # Plot 4
+    obj_data4 = session.execute("SELECT daytime, billVpp1, billVpp2, billVpp3 FROM electrbill ORDER BY daytime DESC").fetchall()
+    bill_vpp1 = [[int(item[0].timestamp()) * 1000, item[1]] for item in obj_data4]
+    bill_vpp2 = [[int(item[0].timestamp()) * 1000, item[2]] for item in obj_data4]
+    bill_vpp3 = [[int(item[0].timestamp()) * 1000, item[3]] for item in obj_data4]
+
+    context = {
+        'data_v10': data_v10,
+        'data_v11': data_v11,
+        'data_v12': data_v12,
+        'data_v13': data_v13,
+        'bill_vpp1': bill_vpp1,
+        'bill_vpp2': bill_vpp2,
+        'bill_vpp3': bill_vpp3,
+    }
+
+    return render_template("main/index.html", **context)
